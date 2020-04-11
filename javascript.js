9c14cb17c15121af078b46bfdd0e689d6e708552
JoshuaShelswell <61985684+JoshuaShelswell@users.noreply.github.com>
2020-04-10 23:38:10 -0400

     

--- javascript.js
+++ javascript.js
@@ -16,10 +16,10 @@ function showSlides(n) {
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (var i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
